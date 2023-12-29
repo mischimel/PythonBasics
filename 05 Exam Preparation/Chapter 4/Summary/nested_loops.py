6abaef80333ebac505b5afa7e9c57454ae42c4d7
mischimel <michele.fille@students.fhnw.ch>
2023-12-29 19:43:50 +0100

    exam prep CH 4

--- 05 Exam Preparation/Chapter 4/Summary/nested_loops.py
+++ 05 Exam Preparation/Chapter 4/Summary/nested_loops.py
@@ -1,10 +1,10 @@
 # Nested Loops
 
 # Example of a nested loop to print a multiplication table
-for i in range(1, 6):  # Outer loop for the rows
-    for j in range(1, 11):  # Inner loop for the columns
+for i in range(1, 4):  # Outer loop for the rows
+    for j in range(1, 6):  # Inner loop for the columns
         product = i * j
-        print(f"{i} * {j} = {product}", end="\t")  # Print in tab-separated format
+        print(f"{i}*{j}={product}", end="\t")  # Print in tab-separated format
     print()  # Move to the next line after completing inner loop
 
 # End of the program
